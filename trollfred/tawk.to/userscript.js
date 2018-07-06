332bba86cdaf76cc0995f30adc0f2986e898d4f5
Igor Savchuk <trollfred@gmail.com>
2018-07-06 15:44:55 +0200

    add unread activity
    mostly for internal messages

--- trollfred/tawk.to/userscript.js
+++ trollfred/tawk.to/userscript.js
@@ -7,7 +7,11 @@ setInterval( function () {
 
     var total = unopened_chats_count + unseen_messages_count
 
+    var unread_highlights = document.querySelectorAll('#left-panel .waiting, #left-panel .flash-new-message')
+    var unread_activity = unread_highlights.length > 0
+
     wavebox.badge.setCount(total)
+    wavebox.badge.setHasUnreadActivity(unread_activity)
 }, 1000)
 
 function read_counter (counter) {
